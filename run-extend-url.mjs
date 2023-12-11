28b6b7461d0e1b72b0bdd2fad61d861525f1719e
Warat Wongmaneekit <thangman22@users.noreply.github.com>
2023-12-11 23:40:12 +0700

    Upgrade csv libs

--- run-extend-url.mjs
+++ run-extend-url.mjs
@@ -1,41 +1,31 @@
 import extendLink from './libs/extendLink.mjs'
+import csv from 'csvtojson'
 import fs from 'fs'
 
-// quick and dirty CSV parsing, keep only first column for now
-function csvToUrlList(csv) {
-  const rows = csv.split("\n").slice(1);  // remove header
-  const result = [];
-
-  for (const row of rows) {
-    const values = row.split(",")[0];  // keep only first column
-    result.push(values);
-  }
-
-  return result
-}
-
 function shuffle (array) {
-  let currentIndex = array.length; let randomIndex
+  let currentIndex = array.length
+  let randomIndex
   while (currentIndex > 0) {
     randomIndex = Math.floor(Math.random() * currentIndex)
     currentIndex--;
     [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex], array[currentIndex]]
+      array[randomIndex],
+      array[currentIndex]
+    ]
   }
 
   return array
 }
 
-const csv = fs.readFileSync('./auditDomains.csv', 'utf8')
-const urls = csvToUrlList(csv)
+const urlsList = await csv().fromFile('./auditDomains.csv')
 
 fs.writeFileSync('./auditUrls.csv', '')
 fs.appendFileSync('./auditUrls.csv', 'Domain, Link, Type\n')
-for (const url of urls) {
-  console.log(`Extend ${url}`)
-  const extendedUrl = await extendLink(url)
-  fs.appendFileSync('./auditUrls.csv', `${url}, https://${url}, Based\n`)
+for (const url of urlsList) {
+  console.log(`Extend ${url.domain}`)
+  const extendedUrl = await extendLink(url.domain)
+  fs.appendFileSync('./auditUrls.csv', `${url.domain}, https://${url.domain}, Based\n`)
   for (const link of shuffle(extendedUrl).slice(0, 15)) {
-    fs.appendFileSync('./auditUrls.csv', `${url}, ${link}, Extendedlink\n`)
+    fs.appendFileSync('./auditUrls.csv', `${url.domain}, ${link}, Extendedlink\n`)
   }
 }
